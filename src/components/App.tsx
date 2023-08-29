7a588f721881b31a67399d02b10b31cf0535f320
VDromaretska <viktoriya.dromaretska@gmail.com>
2023-08-29 17:17:23 +0200

    Added useState for pasteItem and useEffect.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Header } from "./Header";
 import { NewPaste } from "./NewPaste";
 import { RecentPasteList } from "./RecentPasteList";
+import axios from "axios";
+
+const baseUrl = "https://pastebin-upf5.onrender.com";
+// process.env.NODE_ENV === "production"
+//     ? "https://pastebin-upf5.onrender.com"
+//     : "http://localhost:4000";
+
+interface PasteItem {
+    id: number;
+    title: string;
+    description: string;
+    posted_time: Date;
+}
 
 function App() {
+    const [pasteList, setPasteList] = useState<PasteItem[]>([]);
+    useEffect(() => {
+        fetchAndStorePastes();
+    }, []);
+    async function fetchAndStorePastes() {
+        try {
+            const response = await axios.get(baseUrl + "/pastes");
+            console.log(response.data, response.status);
+            const responseData: PasteItem[] = response.data;
+            setPasteList(responseData);
+        } catch (error) {
+            console.log("Error fetching data", error);
+        }
+    }
     return (
         <div className="App">
             <Header />
