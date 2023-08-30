4e2364261a9361af45f867b79bedcec5f1ce3aa4
Adil Rahman <arahman.99@outlook.com>
2023-08-30 16:10:49 +0100

    added dynamic summary view

--- src/components/PasteView.tsx
+++ src/components/PasteView.tsx
@@ -1,14 +1,34 @@
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
+import { useState } from "react";
 import { PasteItem } from "./App";
+import "./pasteView.css";
 
 interface PasteViewProps {
     pasteItem: PasteItem;
 }
 
 export function PasteView({ pasteItem }: PasteViewProps): JSX.Element {
+    const [isDescriptionClicked, setIsDescriptionClicked] =
+        useState<boolean>(false);
+
+    function handleDescriptionClick() {
+        setIsDescriptionClicked((prev) => (prev === true ? false : true));
+    }
     return (
         <div>
             <h2>{pasteItem.title}</h2>
-            <p>{pasteItem.description}</p>
+            <div className="summary-container">
+                {!isDescriptionClicked && (
+                    <p onClick={handleDescriptionClick}>
+                        {pasteItem.description}
+                    </p>
+                )}
+            </div>
+            {isDescriptionClicked && (
+                <p onClick={handleDescriptionClick}>{pasteItem.description}</p>
+            )}
         </div>
     );
 }
